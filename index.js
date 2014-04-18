75f9696a7b3e754b5d315d6e54b83bf59fa3b925
Lennart Melzer <me@lmaa.name>
2014-04-18 23:39:25 +0200

    [fix] returnExport -> debounce

--- index.js
+++ index.js
@@ -20,29 +20,29 @@
         module.exports = factory();
     } else {
         // Browser globals (root is window)
-        root.returnExports = factory();
+        root.debounce = factory();
   }
 }(this, function () {
 
     return function debounce(func, threshold, execAsap){
       var timeout;
-    
+
       return function debounced(){
         var obj = this, args = arguments;
-    
+
         function delayed () {
           if (!execAsap) {
             func.apply(obj, args);
           }
           timeout = null;
         }
-    
+
         if (timeout) {
           clearTimeout(timeout);
         } else if (execAsap) {
           func.apply(obj, args);
         }
-    
+
         timeout = setTimeout(delayed, threshold || 100);
       };
     };
